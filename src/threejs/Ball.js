8af1e8baf8c5f7a04e238ab9573cc7a3ff045c3e
rushabh-01 <rushabhbhuptani09@gmail.com>
2023-04-01 16:28:02 +0530

    changes

--- src/threejs/Ball.js
+++ src/threejs/Ball.js
@@ -1,32 +1,23 @@
 import React,{Suspense} from 'react'
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls,Decal,Float,Preload,useTexture } from '@react-three/drei';
+import { OrbitControls, Float, Preload,Decal, useTexture } from '@react-three/drei';
 import CanvasLoader from '../components/Loader';
 
-const Ball = (props) => {
-    const [decal] = useTexture([props.imgUrl]);
+const Ball = ({ imgUrl }) => {
+  const [decal] = useTexture([imgUrl]);
   return (
     <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-        <ambientLight intensity={0.25}>
-            <directionalLight position={[0,0,0.25]}>
-                <mesh castShadow receiveShadow scale={2.75}>
-                    <icosahedronGeometry args={[1,1,]} />
-                    <meshStandardMaterial 
-                    color='#fff8eb'
-                    polygonOffset
-                    polygonOffsetFactor={-5}
-                    flatShading
-                    />
-                    <Decal 
+      <mesh scale={2.75}>
+        <sphereGeometry args={[1, 32, 32]} />
+        {/* <meshBasicMaterial  /> */}
+        <Decal 
                     rotation={[2 * Math.PI, 0, 6.25]}
                     scale={1}
                     flatShading
                     position={[0,0,1]}
                     map={decal}
                     />
-                </mesh>
-            </directionalLight>
-        </ambientLight>
+      </mesh>
     </Float>
   )
 }
@@ -34,17 +25,22 @@ const Ball = (props) => {
 const BallCanvas = ({icon}) => {
     return (
         <Canvas
-      frameloop='demand'
-      dpr={[1, 2]}
-      gl={{ preserveDrawingBuffer: true }}
-    >
-      <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls enableZoom={false} />
-        <Ball imgUrl={icon} />
-      </Suspense>
+            frameloop='demand'
+            dpr={[1, 2]}
+            gl={{ preserveDrawingBuffer: true }}
+            camera={{ position: [0, 0, 4], fov: 50 }}
+            style={{ height: '100%', width: '100%' }}
+        >
+            <Suspense fallback={<CanvasLoader />}>
+                <OrbitControls enableZoom={false} />
+                <Ball imgUrl={icon} />
+            </Suspense>
 
-      <Preload all />
-    </Canvas>
+            <ambientLight intensity={0.15} />
+            <pointLight position={[10, 10, 10]} intensity={0.5} />
+
+            <Preload all />
+        </Canvas>
     )
 }
 
